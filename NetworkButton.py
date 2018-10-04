ac77051a011e146a68458808ee303dee054c5743
Naaro <36118977+Naaro@users.noreply.github.com>
2018-10-03 19:18:20 -0700

    Update NetworkButton.py

--- NetworkButton.py
+++ NetworkButton.py
@@ -1,31 +1,51 @@
 import RPi.GPIO as GPIO
 import time
+import socket
+
+# GPIO Stuff
+GPIO.setwarnings(false)
 GPIO.setmode(GPIO.BOARD)
-
 Button=3
-LED=5
-BPress=False
-
+WhiteLED=5
 GPIO.setup(Button,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(LED,GPIO.OUT)
 GPIO.output(LED,False)
+
+
+# Network Stuff
+s = socket.socket()
+host = '192.168.0.23'
+port = 9001
+Enabled = True
+
+
+#Begin
+print ('Listener Started!')
+print ('Waiting for Connection to Command...')
+
+s.bind((host,port))
+s.listen(5)
+c, addr = s.accept()
+print ('Connection Established : ',addr)
+print(' ')
 
 
 try: 
     while True:
         if GPIO.input(Button)==0 and BPress==False:
             print('Power : On')
+            c.send(b'LED Power On')
             BPress=True
-            GPIO.output(LED,True)
             time.sleep(0.3)
         elif GPIO.input(Button)==0 and BPress==True:
             print('Power : Off')
+            c.send(b'LED Power On')
             BPress=False
             GPIO.output(LED,False)
             time.sleep(0.3)
         time.sleep(0.1)
-        
 finally:
     # Reset GIP Pins to a safe state
+    c.close()
     GPIO.output(LED,False)
     GPIO.cleanup()
